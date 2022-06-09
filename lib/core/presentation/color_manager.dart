ef7c86ffa3f50beaf97fb76cd039589916872dd0
Omar Assem <omarassem996@gmail.com>
2022-06-10 00:25:54 +0200

    add core presentation layer
    - add color manager
    - add font manager
    - add values manager
    - add style manager

---
+++ lib/core/presentation/color_manager.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  Color primary = const Color(0xff000000);
+}
---
+++ lib/core/presentation/values_manager.dart
@@ -0,0 +1,26 @@
+class AppMargin {
+  static const m8 = 8.0;
+  static const m12 = 12.0;
+  static const m14 = 14.0;
+  static const m16 = 16.0;
+  static const m18 = 18.0;
+  static const m20 = 20.0;
+}
+
+class AppPadding {
+  static const p8 = 8.0;
+  static const p12 = 12.0;
+  static const p14 = 14.0;
+  static const p16 = 16.0;
+  static const p18 = 18.0;
+  static const p20 = 20.0;
+}
+
+class AppSize {
+  static const s8 = 8.0;
+  static const s12 = 12.0;
+  static const s14 = 14.0;
+  static const s16 = 16.0;
+  static const s18 = 18.0;
+  static const s20 = 20.0;
+}
