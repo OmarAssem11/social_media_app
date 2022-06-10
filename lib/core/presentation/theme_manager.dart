9914aa6fd006290e1e43e87263b17fc20ffb0357
Omar Assem <omarassem996@gmail.com>
2022-06-10 02:42:54 +0200

    add card & appBar & button themes
    - add card theme
    - add appBar theme
    - add button theme
    - add elevatedButton theme

--- lib/core/presentation/theme_manager.dart
+++ lib/core/presentation/theme_manager.dart
@@ -1,9 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:social_media_app/core/presentation/color_manager.dart';
+import 'package:social_media_app/core/presentation/font_manager.dart';
+import 'package:social_media_app/core/presentation/styles_manager.dart';
+import 'package:social_media_app/core/presentation/values_manager.dart';
 
 ThemeData getAppTheme() => ThemeData(
       primaryColor: ColorManager.primary,
       primaryColorLight: ColorManager.lightPrimary,
       primaryColorDark: ColorManager.darkPrimary,
       disabledColor: ColorManager.grey1,
+      splashColor: ColorManager.lightPrimary,
+      cardTheme: const CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      ),
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.lightPrimary,
+        titleTextStyle: getRegularTextStyle(
+          fontSize: FontSizeManager.s16,
+          color: ColorManager.white,
+        ),
+      ),
+      buttonTheme: const ButtonThemeData(
+        shape: StadiumBorder(),
+        disabledColor: ColorManager.grey1,
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.lightPrimary,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          textStyle: getRegularTextStyle(
+            color: ColorManager.white,
+            fontSize: FontSizeManager.s18,
+          ),
+          primary: ColorManager.primary,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(AppSize.s12)),
+          ),
+        ),
+      ),
     );
